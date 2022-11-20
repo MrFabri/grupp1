1f929079721ca45544094bb780d9d97908cb8518
3xer <pontuslundholm1@gmail.com>
2022-11-20 22:45:35 +0100

    added live update to search but no filtering

--- textFilter.js
+++ textFilter.js
@@ -1,4 +1,3 @@
-
 //sl == searchLength lsl == lastSearchLengh
 var sl = 0;
 var lsl = 0;
@@ -10,23 +9,26 @@ const out = document.getElementById('h1');
   console.log(challenge.title)
 })
 */
-
-displayRooms();
 async function displayRooms() {
     const res = await fetch("https://lernia-sjj-assignments.vercel.app/api/challenges%27");
     const data = await res.json();
     data.challenges.forEach(challenge => {
         console.log(challenge.title)
     });
-}
+    }
 
 searchInput.addEventListener("input", function(){
+    
     sl = searchInput.innerHTML.length;
     if(sl < maxLine){
         const searchInput = document.querySelector('.input-search-keyword');
         var searchString = searchInput.value;
         out.innerHTML = searchString;
+        displayRooms();
         
     }
+    
+    
+        
 })
 
