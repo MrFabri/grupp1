899a315d208ee12ed7ed50eb76011b4fe3ffc2ec
Vedad <vedadz@gmail.com>
2022-11-23 17:16:05 +0100

    Made display functions and polished
    Added function for online/onsite.

--- app.js
+++ app.js
@@ -5,55 +5,102 @@ mainNavToggle.addEventListener('click', () => {
     mainNav.classList.toggle('open');
 });
 
+const buttons = document.querySelectorAll("a.button.primary");
+buttons.forEach(button => {
+    button.addEventListener("click", (ev) => {
+        console.log("funkar jag");
+        document.location.href = "http://127.0.0.1:5500/challenges.html";
+        document.body.onload = displayOnlineRooms();
+    })
+})
+
+
+function wellHello() {
+    console.log("hej");
+    displayAllRooms();
+}
 
 const main = document.querySelector("main");
 const roomSection = document.createElement("section");
-const challengesList = document.createElement("ul");
-challengesList.style.columns = 3;
-challengesList.style.listStyle = "none";
-
+const challengesHtmlList = document.createElement("ul");
+challengesHtmlList.style.content = "";
+challengesHtmlList.style.columns = 3;
+challengesHtmlList.style.listStyle = "none";
 
 //Visar alla items på skärmen.
 
-async function displayRooms() {
+async function displayAllRooms() {
     const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
     const data = await res.json();
     data.challenges.forEach(challenge => {
-        createChallengeItem(challenge.image, challenge.title, challenge.rating, challenge.minParticipants, challenge.maxParticipants, challenge.description, challenge.type);
+        createChallengeItem(challenge);
     });
-    roomSection.appendChild(challengesList);
-    challengesList.className = "challenge-list";
-    roomSection.className = "challenges";
+    roomSection.appendChild(challengesHtmlList);
+    roomSection.style.marginTop = "2em";
+    main.appendChild(roomSection);
+}
+
+//Visar online rooms
+async function displayOnlineRooms() {
+    const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await res.json();
+    data.challenges.forEach(challenge => {
+        if (challenge.type == "online") {
+            console.log(challenge.title);
+            createChallengeItem(challenge);
+        }
+    });
+    roomSection.appendChild(challengesHtmlList);
+    roomSection.style.marginTop = "2em";
+    main.appendChild(roomSection);
+}
+
+//Visar onsite rooms 
+async function displayOnSiteRooms() {
+    const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await res.json();
+    data.challenges.forEach(challenge => {
+        if (challenge.type == "onsite") {
+            console.log(challenge.title);
+            createChallengeItem(challenge);
+        }
+    });
+    roomSection.appendChild(challengesHtmlList);
     roomSection.style.marginTop = "2em";
     main.appendChild(roomSection);
 }
 
 // Skapar och lägger till ett item av datan som hämtas från API:et.
 
-function createChallengeItem(challengeImg, title, rating, minParticipants, maxParticipants, description, type) {
+function createChallengeItem(challenge) {
     const challengeItem = document.createElement("li");
+    const tempDiv = document.createElement("div");
     const roomImage = document.createElement("img");
-    roomImage.src = challengeImg;
-    challengeItem.appendChild(roomImage);
+    roomImage.src = challenge.image;
+    tempDiv.appendChild(roomImage);
+    createStars(tempDiv, challenge.rating);
     const roomTitle = document.createElement("h3");
-    roomTitle.textContent = title;
-    roomTitle.classList.add("challenge-title");
-    challengeItem.appendChild(roomTitle);
-    createStars(challengeItem, rating);
+    roomTitle.textContent = challenge.title;
+    roomTitle.style.paddingLeft = "1em";
+    roomTitle.style.margin = 0;
+    tempDiv.appendChild(roomTitle);
     const participants = document.createElement("small");
-    participants.textContent = minParticipants + "-" + maxParticipants + " participants";
+    participants.textContent = challenge.minParticipants + "-" + challenge.maxParticipants + " participants";
+    participants.style.paddingLeft = "1em";
     participants.classList.add("challenge-meta");
-    challengeItem.appendChild(participants);
+    tempDiv.appendChild(participants);
     const challengeDescription = document.createElement("p");
-    challengeDescription.textContent = description;
+    challengeDescription.textContent = challenge.description;
     challengeDescription.style.margin = "0.5em 0";
     challengeDescription.classList.toggle("challenge-description");
-    challengeItem.appendChild(challengeDescription);
-    createButton(challengeItem, type);
-    styleCards(challengeItem);
-    challengesList.appendChild(challengeItem);
+    challengeDescription.style.paddingLeft = "1em";
+    tempDiv.appendChild(challengeDescription);
+    createButton(tempDiv, challenge.type);
+    styleCards(tempDiv);
+    challengeItem.appendChild(tempDiv);
+    challengesHtmlList.appendChild(challengeItem);
 }
-//funkar detdsadsad
+
 // skapar antalet stjärnor
 
 function createStars(challengeItem, rating) {
@@ -68,6 +115,9 @@ function createStars(challengeItem, rating) {
         }
         stars.appendChild(star);
     }
+    stars.style.position = "relative";
+    stars.style.top = "-1.5em";
+    stars.style.paddingLeft = "1em";
     challengeItem.appendChild(stars);
 }
 
@@ -76,6 +126,7 @@ function createStars(challengeItem, rating) {
 function createButton(challengeItem, challengeType) {
     const button = document.createElement("button");
     button.className = "button primary";
+    button.style.display = "block";
     button.style.margin = "0px 0.7em 0px auto";
     button.style.fontSize = " 0.7em";
     if (challengeType == "onsite") {
@@ -90,7 +141,7 @@ function createButton(challengeItem, challengeType) {
 // stylar korten.
 
 function styleCards(challengeItem) {
-    challengeItem.style.minWidth = "360px";
+    challengeItem.style.minWidth = "260px";
     challengeItem.style.maxWidth = "400px";
     challengeItem.style.borderRadius = "4px";
     challengeItem.style.marginBottom = "1em";
