ead3e3e5a6b98f35c169a211f00422290b508ae3
3xer <pontuslundholm1@gmail.com>
2022-11-29 17:40:17 +0100

    Refined the code and fixed some minor things

--- textFilter.js
+++ textFilter.js
@@ -1,67 +1,63 @@
-// selects the elements in dom as variables
+// selects the elements in DOM
 const searchInput = document.querySelector('.input-search-keyword');
 const roomsUl = document.querySelector('.rooms');
-// Decides if the input is empty or not
-function startOfSearch(){
+
+function search(){
     const preSearchString = searchInput.value;
-    // if preSearchString/input is empty display all rooms
-    if(preSearchString == ''){
-        renderAll;
+    //if string empty display all rooms
+    if(preSearchString.lenght <= 0){
+        toRenderAll;
     }
     //if string is not empty render rooms matching the string
     else{
-        renderWFilter(preSearchString);
+        filter(preSearchString);
     }
 }
+
 // renders all rooms if the input (searchString) is empty
-async function renderAll(){
-    const data = await APIGet();
+async function toRenderAll(){
+    const data = await getChallenges();
     roomsUl.innerText = '';
-    data.challenges.forEach(challenge => {
-        const roomsUl = document.querySelector(".rooms");
-        const item = document.createElement("li");
-        const content = document.createElement("p");
-        content.innerText = "\nTitle: " + challenge.title + "\nDescription: " + challenge.description;
-        item.appendChild(content);
-        roomsUl.appendChild(item);
-    });
-}
-// renders all the rooms that matches their title and or description with searchString.
-async function renderWFilter(preSearchString){
-    const data = await APIGet();
-    roomsUl.innerText = ''; 
-    // makes the searchstring to lowercase to make the search not caseSensitive
-    const searchString = preSearchString.toLowerCase();
 
     data.challenges.forEach(challenge => {
-        /* 
-        makes the title and description to lowercase to make the search not caseSensitive
-        without the actual title and description changing
-        LC == LowerCase
-        */
+        render(challenge.id, challenge.title, challenge.description);
+    });
+}
+
+// renders all the rooms that matches their title and or description with searchString.
+function render(id, title, description){
+    const item = document.createElement("li");
+    const content = document.createElement("p");
+    content.innerText = id + "\nTitle: " + title + "\nDescription: " + description;
+    item.appendChild(content);
+    roomsUl.appendChild(item);
+}
+
+async function filter(preSearchString){
+    const data = await getChallenges();
+    // makes the preSearchString to lowercase so the search ignores if you use upper or lower case
+    const searchString = preSearchString.toLowerCase();
+    roomsUl.innerText = ''; 
+    data.challenges.forEach(challenge => {
+        // makes the title and description to lowercase to make the search not caseSensitive LCTitle/LCDescription == LowerCaseTitle/Description 
         const LCTitle = challenge.title.toLowerCase();
         const LCDescription = challenge.description.toLowerCase();
         
         if(LCTitle.includes(searchString) || LCDescription.includes(searchString)){
-            const roomsUl = document.querySelector(".rooms");
-            const item = document.createElement("li");
-            const content = document.createElement("p");
-            content.innerText = "\nTitle: " + challenge.title + "\nDescription: " + challenge.description;
-            item.appendChild(content);
-            roomsUl.appendChild(item);
+            render(challenge.id, challenge.title, challenge.description);
         }        
     });
-    // if no room match show a message that no rooms match the search
+     // if no room match show a message that no rooms match the search
     if(roomsUl.innerHTML == ''){
         roomsUl.innerHTML = 'no matching rooms';
     }
 }
+
 // fetches the data from the api 
-async function APIGet(){
+async function getChallenges(){
     const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
     const data = await res.json();
     return data;
 }
-// render all rooms when the page is loaded 
-renderAll();
-searchInput.addEventListener("keyup", startOfSearch);
+toRenderAll();
+searchInput.addEventListener("keyup", search);
