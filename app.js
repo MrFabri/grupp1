fe81af538774e06cb47bd910397a2869c3b6c5ba
3xer <57897265+3xer@users.noreply.github.com>
2023-03-08 12:32:35 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-import express from "express";
-
-const app = express()
-const PORT = process.env.PORT || 3080
-
-app.get('/', (req, res) => {res.send ('Yes, it works!');});
-app.listen(PORT, () => console.log(`Server listeningon port: ${PORT}`));
